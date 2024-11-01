7821efe8c3b7581ffc74af4c7f98db499bf9767e
painmybrain <igrovaya.si@gmail.com>
2024-11-01 16:45:10 +0300

    add condition render

--- rate-cards/src/components/Card.jsx
+++ rate-cards/src/components/Card.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import styles from './Card.module.css'
 
 function Card({ props }) {
+  const [clicked, setClicked] = useState('card_container');
   const { name, price, speed, description } = props;
-  return (
 
-    <div className={styles.card_container}>
+  function getClicked() {
+    setClicked('card_container_selected')
+  }
+
+
+  return (
+    <div
+      onClick={getClicked}
+      className={styles[clicked]}>
       <div className={styles.card_name}>{name}</div>
       <div className={styles.card_price}>{price}</div>
       <div className={styles.card_speed}>{speed}</div>
